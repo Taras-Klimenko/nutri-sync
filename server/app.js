9d57d547f5825ccb620781ca2f1971bdb36b5157
Albina <sataeva.albina2013@yandex.ru>
2023-11-28 14:48:58 +0300

    change

--- server/app.js
+++ server/app.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
+const logger = require('morgan');
 const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3100;
+
+const clientRouter = require('./routers/clientRouter');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Работает');
+});
+
+app.use('/clients', clientRouter);
+
+app.use('/clients/:id', clientRouter);
+
+app.use('client/:id/param', clientRouter);
+
+app.use('*', (req, res) => {
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
