5db4adca982a04a6fd3f6a7c2a43c7711ee733a6
Taras-Klimenko <tarasklimenko.1989@gmail.com>
2023-12-05 14:04:12 +0300

    Dashboard fix - pull only assigned clients

--- client/src/components/AllClients/AllClients.tsx
+++ client/src/components/AllClients/AllClients.tsx
@@ -1,28 +1,42 @@
-import MyButton from "../MyButton/MyButton.tsx";
-import { useAppSelector } from "../../redux/store/hooks.ts";
-import { Link } from "react-router-dom";
+import MyButton from '../MyButton/MyButton.tsx';
+import { Link } from 'react-router-dom';
 import styles from './AllClients.module.css'; // Замените на путь к вашему файлу стилей
+import { useAppDispatch, useAppSelector } from '../../redux/store/hooks.ts';
+import { getClients } from '../../redux/store/thunkActions.ts';
+import { useEffect } from 'react';
 
 export default function AllClients() {
-    const { clients } = useAppSelector((store) => store.clientSlice);
-    return (
-        <div className={styles.allClients}>
-            <h2>All Clients</h2>
+  const dispatch = useAppDispatch();
+  const { clients } = useAppSelector((store) => store.clientSlice);
+  const { login } = useAppSelector((store) => store.userSlice);
+  const filteredCliens = clients.filter((element) => {
+    return element?.Curator?.login === login;
+  });
+
+  useEffect(() => {
+    dispatch(getClients());
+  }, [dispatch]);
+
+  return (
+    <div className={styles.allClients}>
+      <h2>All Clients</h2>
+      <div>
+        {filteredCliens.map((client) => (
+          <div key={client.id} className={styles.clientItem}>
             <div>
-                {clients.map((client) => (
-                    <div key={client.id} className={styles.clientItem}>
-                       <div>{client.firstName} {client.lastName}</div>
-                        <div>
-                            <Link to={`/clients/${client.id}`} state={{ id: client.id }}>
-                                <MyButton>Info</MyButton>
-                            </Link>
-                            <Link to={`/client/${client.id}`} state={{ id: client.id }}>
-                                <MyButton>Redact</MyButton>
-                            </Link>
-                        </div>
-                    </div>
-                ))}
+              {client.firstName} {client.lastName}
             </div>
-        </div>
-    );
+            <div>
+              <Link to={`/clients/${client.id}`} state={{ id: client.id }}>
+                <MyButton>Info</MyButton>
+              </Link>
+              <Link to={`/client/${client.id}`} state={{ id: client.id }}>
+                <MyButton>Redact</MyButton>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
