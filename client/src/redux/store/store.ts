d68d0537bb02f85545e5f332845d5a4ad0a3f1ca
Taras-Klimenko <tarasklimenko.1989@gmail.com>
2023-12-08 12:26:20 +0300

    editor style fixes, local storage fix

--- client/src/redux/store/store.ts
+++ client/src/redux/store/store.ts
@@ -1,9 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { clientSlice } from './slice/clientSlice.ts';
 import { userSlice } from './slice/userSlice.ts';
+import { saveToLocalStorage, loadFromLocalStorage } from './localStorage.js';
+
+const preloadedState = loadFromLocalStorage();
 
 export const store = configureStore({
   reducer: { clientSlice: clientSlice.reducer, userSlice: userSlice.reducer },
+  preloadedState,
+});
+
+// store.subscribe(() => {
+//   saveToLocalStorage({
+//     userSlice: store.getState().userSlice,
+//   });
+// });
+
+store.subscribe(() => {
+  const userState = store.getState().userSlice;
+  if (userState && userState.login) {
+    saveToLocalStorage({ userSlice: userState });
+  }
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
